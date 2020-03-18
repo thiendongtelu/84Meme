b1accb24bce0a15a5c237cbe7512f6475fdb7d64
thiendongtelu <59526587+thiendongtelu@users.noreply.github.com>
2020-03-18 15:34:10 +0700

    initial commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send('hello');
 });
 
+app.get('/api/helloworld', (req, res) => {
+  res.json({sayHi: 'hello from server, nice to meet you!'})
+})
+
 app.listen(5000,()=>{
     console.log('App listening on port 5000')
 });
