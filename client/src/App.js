f9e049cf3e6c3752410cd96dfa3d57820eccb4b6
thiendongtelu <59526587+thiendongtelu@users.noreply.github.com>
2020-03-19 12:31:44 +0700

    test server

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 
+// const toBase64 = file => new Promise((resolve, reject) => {
+//   const reader = new FileReader();
+//   reader.readAsBinaryString(file);
+//   reader.onload = () => resolve(reader.result);
+//   reader.onerror = error => reject(error);
+// });
+
 class App extends React.Component {
+
+  send = async () => {
+
+    try {
+      const formData = new FormData();
+      const image = document.querySelector('input').value;
+      console.log(image);
+      axios.post('/img', {image}).then(result=>console.log(result.data));
+      
+      // axios.post(`https://api.imgbb.com/1/upload?key=430f22ae6e75ba471e67081eec99d78b&image=${file}`)
+      //   .then(result => console.log(result))
+      //   .catch(error => console.log(error));
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
 
   componentDidMount() {
     axios.get('/api/helloworld')
       .then(result => console.log(result))
       .catch(error => console.log(error));
+      
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type='text' />
+        {/* <input type='file' /> */}
+        <button onClick={this.send}>Send</button>
       </div>
     );
   }
--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('hello');
+app.use(express.json());
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('hello');
 });
 
-app.get('/api/helloworld', (req, res) => {
-  res.json({sayHi: 'hello from server, nice to meet you!'})
+app.post('/img', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 })
 
-app.listen(5000,()=>{
-    console.log('App listening on port 5000')
+app.get('/api/helloworld', (req, res) => {
+  res.json({ sayHi: 'hello from server, nice to meet you!' })
+})
+
+
+app.listen(5000, () => {
+  console.log('App listening on port 5000')
 });
